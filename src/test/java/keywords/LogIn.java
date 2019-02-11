7a9c089af1914d5fee032d17e58ee18a6352ddc8
Takács Róbert <89.t.robert@gmail.com>
2019-02-11 11:17:00 +0100

    hehehe

--- src/test/java/keywords/LogIn.java
+++ src/test/java/keywords/LogIn.java
@@ -21,7 +21,7 @@ public class LogIn {
   }
 
   public static void logIn(WebDriver driver, String userName, String password, SystemDefaults defaults) {
-    driver.get(defaults.getUrl());
+    driver.get("http://" + defaults.getUrl());
     wait = new WebDriverWait(driver, defaults.getWaitTimeOut());
 
     logInUserName = wait.until(ExpectedConditions.elementToBeClickable(By.id("login-form-username")));
