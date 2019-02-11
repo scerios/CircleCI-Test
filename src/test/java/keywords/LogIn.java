0edf0a406e121f2e37790d557cb92f87d15faec9
Takács Róbert <89.t.robert@gmail.com>
2019-02-11 12:01:10 +0100

    revert

--- src/test/java/keywords/LogIn.java
+++ src/test/java/keywords/LogIn.java
@@ -21,7 +21,7 @@ public class LogIn {
   }
 
   public static void logIn(WebDriver driver, String userName, String password, SystemDefaults defaults) {
-    driver.get("http://jira.greenfox.codecanvas.hu");
+    driver.get("https://" + defaults.getUrl() + "/secure/Dashboard.jspa");
     wait = new WebDriverWait(driver, 10);
 
     logInUserName = wait.until(ExpectedConditions.elementToBeClickable(By.id("login-form-username")));
